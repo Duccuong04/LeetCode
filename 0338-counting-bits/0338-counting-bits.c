91810c8425a7ef77f3a5daa78e1ca07baf047adf
Nguyen Duc Cuong <duccuonggg04@gmail.com>
2025-06-12 16:03:54 +0700

    Time: 0 ms (100%), Space: 14.9 MB (95.51%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+int* countBits(int n, int* returnSize) {
+    *returnSize = n + 1;
+    int* result = (int*) malloc((n + 1) * sizeof(int));
+    result[0] = 0;
+    for (int i = 1; i <= n; i++) {
+        result[i] = result[i >> 1] + (i & 1);
+    }
+    return result;
+}
