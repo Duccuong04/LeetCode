c15584a466abc7963a9d7bf40ff41b0f31fa77ec
Nguyen Duc Cuong <duccuonggg04@gmail.com>
2025-06-12 16:37:02 +0700

    Time: 2 ms (38.1%), Space: 15 MB (23.13%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.c
+++ 0338-counting-bits/0338-counting-bits.c
@@ -1,3 +1,17 @@
+/*
+Dynamic Programming:
+
+Bài toán có thể chia nhỏ thành các bài toán con.
+
+Các bài toán con này bị lặp lại nhiều lần.
+
+Có sự phụ thuộc giữa các bài toán con.
+
+result[i] = result[i/2] + (i%2); // dựa vào kết quả đã tính trước đó và xét bit cuối là 0 hay 1
+
+Ví dụ: 10 = 1010; Xét 10/2 = 5 -> result[5] + 1010 & 0001 (xét thêm bit cuối là 0 hay 1)
+*/
+
 #include <stdlib.h>
 
 int* countBits(int n, int* returnSize) {
